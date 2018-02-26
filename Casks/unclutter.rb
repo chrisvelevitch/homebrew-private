ec221d894b67dd6c33cf9572fc469036fe94d707
Chris Velevitch <chris.velevitch@thefoundry.co.uk>
2018-02-27 02:39:28 +0800

    Inital commit

--- Casks/unclutter.rb
+++ Casks/unclutter.rb
@@ -1,5 +1,6 @@
 cask 'unclutter' do
   version '2.1.6d'
+  sha256 'fae6cd6e631f27cdef26cc905266f1068cb50e566eac35bb0fb646a4e14948b8'
   
   url "https://www.unclutterapp.com/files/Unclutter.zip"
   name 'Unclutter'
